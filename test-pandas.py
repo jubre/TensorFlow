134b70c55c24de1dafaebbeb906fd1d95ec50776
Junior Corman <junior.corman@gmail.com>
2018-04-13 15:09:35 -0500

    Creando un Data Frame

--- test-pandas.py
+++ test-pandas.py
@@ -44,3 +44,8 @@ print(df['last_name'])
 # Creando Series
 s = pd.Series([1, 2, 4, 5, 7, 8, 10])
 print(s)
+
+# Creando un Data DataFrame
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+print(df)
