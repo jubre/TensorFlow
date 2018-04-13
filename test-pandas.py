1316e216030610783ef06a39df6b989047417354
Junior Corman <junior.corman@gmail.com>
2018-04-13 15:13:57 -0500

    Data Frame Index

--- test-pandas.py
+++ test-pandas.py
@@ -51,3 +51,6 @@ print(s)
 dates = pd.date_range('20130101', periods=6)
 df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
 print(df)
+
+print("Data Frame Index: ")
+print(df.index)
