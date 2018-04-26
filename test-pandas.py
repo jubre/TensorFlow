f1b53f795047a856644dc251b82a093070053f1d
Junior Corman <junior.corman@gmail.com>
2018-04-26 15:26:29 -0500

    Doing append

--- test-pandas.py
+++ test-pandas.py
@@ -196,3 +196,9 @@ merged = pd.merge(left, right, on='key')
 print(merged)
 
 # Append rows to a dataframe
+df = pd.DataFrame(np.random.randn(8, 4), columns=['A','B','C','D'])
+s = df.iloc[3]
+append = df.append(s, ignore_index=True)
+print(append)
+
+# Pivot
