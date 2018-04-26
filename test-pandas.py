a70ad87ffcc7c868c283981007aec47db02a2c27
Junior Corman <junior.corman@gmail.com>
2018-04-26 15:27:37 -0500

    Doing pivot

--- test-pandas.py
+++ test-pandas.py
@@ -202,3 +202,11 @@ append = df.append(s, ignore_index=True)
 print(append)
 
 # Pivot
+df = pd.DataFrame({'A' : ['one', 'one', 'two', 'three'] * 3,
+   'B' : ['A', 'B', 'C'] * 4,
+   'C' : ['foo', 'foo', 'foo', 'bar', 'bar', 'bar'] * 2,
+   'D' : np.random.randn(12),
+   'E' : np.random.randn(12)})
+
+pivot = pd.pivot_table(df, values='D', index=['A', 'B'], columns=['C'])
+print(pivot)
