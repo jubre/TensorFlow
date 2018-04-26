9d56422555862c5e91da4aa8bba9f0a4f0132a24
Junior Corman <junior.corman@gmail.com>
2018-04-26 15:13:18 -0500

    doing merge

--- test-pandas.py
+++ test-pandas.py
@@ -191,3 +191,6 @@ print(pd.concat(pieces))
 # Join
 left = pd.DataFrame({'key': ['foo', 'foo'], 'lval': [1, 2]})
 right = pd.DataFrame({'key': ['foo', 'foo'], 'rval': [4, 5]})
+
+merged = pd.merge(left, right, on='key')
+print(merged)
